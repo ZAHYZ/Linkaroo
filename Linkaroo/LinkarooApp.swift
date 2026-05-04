b3462d4caca9b790fe69acc4a626991c8156f589
ZAHYZ <zahyaanazmi@gmail.com>
2026-05-04 16:13:20 +1000

    Initial Commit

---
+++ Linkaroo/LinkarooApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkarooApp.swift
+//  Linkaroo
+//
+//  Created by Zahyaan Azmi on 4/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct LinkarooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
